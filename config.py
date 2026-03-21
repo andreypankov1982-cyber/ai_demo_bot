1305e84f1bc0d17329a37640e3c143db6355eb6f
andreypankov1982-cyber <andrey.pankov1982@gmail.com>
2026-03-21 17:21:42 +0400

    Исправил опечатку

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
-        case_sensitive=False,  # ← Важно: игнорируем регистр!
+        case_sensitive=False,  
         extra="ignore"
     )
 
